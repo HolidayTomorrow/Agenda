12baba320579ecee41db09aa57cee1ccf94fd84d
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-11-26 19:33:55 -0300

    Comitando todas as alterações necessárias para criar o projeto base do django.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'email',
+    ordering = 'id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 20
+    list_max_show_all = 80
+    list_display_links = 'id', 'first_name', 'email',
+    
+    
+    
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
